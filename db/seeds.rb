23f58da0cf5e2bdb4432dbf524a60e750e6a1ab0
Kōhei Yamamoto <kymmt90@gmail.com>
2017-08-30 00:12:55 +0900

    Generate data by db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users = User.create(
+  [
+    { email: 'foo@example.com', password: 'password' },
+    { email: 'bar@example.com', password: 'password' }
+  ]
+)
+
+users.each do |user|
+  user.snippets.create(
+    [
+      { title: 'title 1', content: 'contents of title !!!!!!!!!!!!!!1' },
+      { title: 'title 2', content: '' },
+      { title: 'タイトル 3', content: '日本語のスニペット' }
+    ]
+  )
+end
