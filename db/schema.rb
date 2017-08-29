5298eeab8aba9fbbc00d75ed33dea08f992e31a0
Kōhei Yamamoto <kymmt90@gmail.com>
2017-08-30 00:07:03 +0900

    Add stars table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170829150209) do
+ActiveRecord::Schema.define(version: 20170829150642) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content", null: false
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 20170829150209) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_snippets_on_user_id"
+  end
+
+  create_table "stars", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "snippet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["snippet_id"], name: "index_stars_on_snippet_id"
+    t.index ["user_id"], name: "index_stars_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
