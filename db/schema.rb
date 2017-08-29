29cfe8f465f2ac3068284f78ec37a02891e432e8
Kōhei Yamamoto <kymmt90@gmail.com>
2017-08-30 00:00:02 +0900

    Add snippets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170829145615) do
+ActiveRecord::Schema.define(version: 20170829145914) do
+
+  create_table "snippets", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "content", null: false
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_snippets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
