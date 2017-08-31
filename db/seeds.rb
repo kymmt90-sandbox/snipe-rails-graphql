bdb7f40d213bac09c0623ba0276dc52064045331
Kōhei Yamamoto <kymmt90@gmail.com>
2017-09-01 00:22:58 +0900

    Seed DB with articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,14 @@ users = User.create(
 )
 
 users.each do |user|
+  user.articles.create(
+    [
+      { title: 'article 1', content: 'contents', publisher: 'hackernews' },
+      { title: 'article 2', content: '', publisher: 'reddit' },
+      { title: '記事 3', content: '日本語の記事', publisher: 'はてな' }
+    ]
+  )
+
   user.snippets.create(
     [
       { title: 'title 1', content: 'contents of title !!!!!!!!!!!!!!1' },
