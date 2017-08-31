e8195d25fb14c4f02e350cf0a982764830ea2f8c
Kōhei Yamamoto <kymmt90@gmail.com>
2017-09-01 00:09:10 +0900

    Add the not-null constraint to article attributes

--- db/migrate/20170831150702_create_articles.rb
+++ db/migrate/20170831150702_create_articles.rb
@@ -1,8 +1,8 @@
 class CreateArticles < ActiveRecord::Migration[5.1]
   def change
     create_table :articles do |t|
-      t.string :title
-      t.string :content
+      t.string :title, null: false
+      t.string :content, null: false
       t.string :publisher
       t.references :user, foreign_key: true
 
